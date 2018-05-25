ed5dad047d2f8f6e50de55c00f5613caa9b0323a
zhoumiaode <1010467470@qq.com>
2018-05-25 18:04:52 +0800

    six

--- src/main/java/com/example/test/controller/FreemarkerController.java
+++ src/main/java/com/example/test/controller/FreemarkerController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -23,5 +24,14 @@ public class FreemarkerController {
         modelMap.addAttribute("girlProperties",girlProperties);
         System.out.println(1);
         return "freemarker";
+    }
+
+    @PostMapping(value = "postFree")
+    public String postFree(GirlProperties girlProperties){
+
+        System.out.println(girlProperties.getName());
+        System.out.println(121221);
+        System.out.println(22222);
+        return "redirect:/free/getFree";
     }
 }
