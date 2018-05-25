85d778db57b9e9be34708e698abdf1265014bf4a
zhoumiaode <1010467470@qq.com>
2018-05-25 17:30:50 +0800

    eee

--- src/main/java/com/example/test/controller/GirlController.java
+++ src/main/java/com/example/test/controller/GirlController.java
@@ -73,7 +73,7 @@ public class GirlController {
     @GetMapping(value="FindById/{id}")
     public Girl GirlFindOne(@PathVariable("id") Integer id){
         System.out.println(1);
-        return girlRepository.findById(id).get();
+        return girlRepository.getOne(id);
     }
 
     @PutMapping(value="update")
