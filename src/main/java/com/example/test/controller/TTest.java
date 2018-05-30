14bcd4d69da16d8145aafa261f8193892a0f70d2
zhoumiaode <1010467470@qq.com>
2018-05-30 11:56:23 +0800

    thirty

---
+++ src/main/java/com/example/test/controller/TTest.java
@@ -0,0 +1,19 @@
+package com.example.test.controller;
+
+import java.util.Scanner;
+
+
+public class TTest {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入一个数组长度：");
+        while(sc.hasNext()){
+            int a=sc.nextInt();//输入一个正整数
+            int b=sc.nextInt();//输入一个正整数
+            int c=a+b;
+            System.out.println("输入的数字是："+c);
+        }
+}
+
+}
