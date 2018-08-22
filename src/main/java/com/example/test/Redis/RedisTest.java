73176659fd10250e420cfa452788ab60b7879640
zhoumiaode <1010467470@qq.com>
2018-08-22 15:44:17 +0800

    zuizhng

--- src/main/java/com/example/test/Redis/RedisTest.java
+++ src/main/java/com/example/test/Redis/RedisTest.java
@@ -1,7 +1,11 @@
 package com.example.test.Redis;
 
+
+import org.apache.log4j.Logger;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 import java.awt.print.Pageable;
 
@@ -18,6 +22,7 @@ import java.awt.print.Pageable;
  * @Version: 1.0
  */
 public class RedisTest {
+
     private JedisPool pool;
 
     public RedisTest(JedisPool pool){
@@ -30,36 +35,47 @@ public class RedisTest {
     * @return: void 
     * @Author: zhoumiaode
     * @Date: 2018/08/21 
-    */ 
+5
+     .
+     */
     public boolean tryLock(String key,long timeout) throws InterruptedException {
 
         Jedis conn=pool.getResource();
         //获取当前时间
-        long time=System.currentTimeMillis();
+        //long time=System.currentTimeMillis();
         while(true){
             //返回1表示获取锁成功
-            if(conn.setnx(key,String.valueOf(time+timeout))==1){
-                //conn.expire
+            if(conn.setnx(key,String.valueOf(System.currentTimeMillis()+timeout))==1){
+                System.out.println(Thread.currentThread().getName()+"我拿到锁了");
+                conn.expire(key,5);
                 return true;
-            }else{
+            }
                 //获取锁的生存时间
-                long time1=getExpire(key);
+                String time1=getExpire(key);
                 //锁的生存时间小于当前时间，则表示锁已经失效
-                if(time1-time<0){
+                long time2=System.currentTimeMillis();
+                if(time1!=null&&(Long.parseLong(time1)-time2<0)){
                     //重新设置锁的生存时间,返回旧的生存时间
-                    String a=conn.getSet(key,String.valueOf(time1+timeout));
+                    //String a=null;
+                    //synchronized (RedisTest.class){
+                    String   a=conn.getSet(key,String.valueOf(System.currentTimeMillis()+timeout));
+                   // }
+
                     //如果旧的生存时间<当前时间，则表示我是第一个进行getset操作，我可以拿到锁，另一种那返回的锁的时候和上面一开始getExpire()
                     //方法获得的时候进行比较，如果相同则表示我是第一个getset的，不相同则表示在我之前已经有人进行过getset操作，那么我只能重新请求。
-                    if(Long.parseLong(a)-time<0){
+                    if(a!=null&&a.equals(time1)&&(Long.valueOf(a)-time2<0)){
+                        conn.expire(key,5);
+                        String time11=getExpire(key);
+                        System.out.println(Thread.currentThread().getName()+"重新设置后锁的值为:"+time11+"//"+a);
+                        System.out.println(Thread.currentThread().getName()+"我是第一个使用getset的人");
                         return true;
                     }else{
-                        System.out.println("正在尝试重新获取锁中");
-                        Thread.sleep(1000);
+                        System.out.println(Thread.currentThread().getName()+"正在尝试重新获取锁中");
+                        Thread.sleep(100);
                     }
                 }
             }
         }
-    }
 
     /** 
     * @Description:获取指定key的生存时间
@@ -68,11 +84,12 @@ public class RedisTest {
     * @Author: zhoumiaode
     * @Date: 2018/08/21 
     */ 
-    public long getExpire(String key){
+    public String getExpire(String key){
         Jedis conn=pool.getResource();
-        long expire=0;
+        String expire=null;
         if(conn.exists(key)){
-            expire=Long.parseLong(conn.get(key));
+            System.out.println(Thread.currentThread().getName()+"key的值为:"+conn.get(key));
+            expire=conn.get(key);
         }
         return expire;
     }
@@ -87,13 +104,21 @@ public class RedisTest {
     public void unlock(String key){
         Jedis conn= pool.getResource();
         //获取当前时间
-        long time=System.currentTimeMillis();
         if(conn.exists(key)){
             //获取锁的时间
             String time1=conn.get(key);
             //如果生存时间大于当前时间，则进行释放锁的操作,如果生存时间小于当前时间，则表示锁已经失效，那么可能已经被别人所占有，则不做操作
-            if(Long.valueOf(time1)-time>0){
+            long time=System.currentTimeMillis();
+            if(time1!=null&&(Long.valueOf(time1)-time>0)){
+                /*Transaction transaction=conn.multi();
+                transaction.del(key);
+                transaction.exec();*/
                 conn.del(key);
+                System.out.println("锁时间："+time1+"/系统时间:"+time);
+                System.out.println(Thread.currentThread().getName()+"我删除锁了");
+            }else{
+                System.out.println("锁时间："+time1+"/系统时间:"+time);
+                System.out.println(Thread.currentThread().getName()+"锁早已经被删除了");
             }
         }
     }
@@ -106,7 +131,7 @@ public class RedisTest {
     * @Author: zhoumiaode
     * @Date: 2018/08/21 
     */ 
-    public void method(){
-        System.out.println("我已经得到锁了，我正在处理自己的事情");
+    public String method(){
+        return"我已经得到锁了，我正在处理自己的事情";
     }
 }
--- src/main/java/com/example/test/Redis/TT.java
+++ src/main/java/com/example/test/Redis/TT.java
@@ -1,5 +1,9 @@
 package com.example.test.Redis;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 import java.text.SimpleDateFormat;
 
 /**
@@ -31,6 +35,20 @@ public class TT {
         System.out.println(currentTime);
         String lockTimeDuration = String.valueOf(currentTime + Lock_Timeout + 1);//锁的持续时间
         System.out.println(lockTimeDuration);
+        JedisPoolConfig config=new JedisPoolConfig();
+        // 设置最大连接数
+        config.setMaxTotal(200);
+        // 设置最大空闲数
+        config.setMaxIdle(8);
+        // 设置最大等待时间
+        config.setMaxWaitMillis(1000 * 1000);
+        // 在borrow一个jedis实例时，是否需要验证，若为true，则所有jedis实例均是可用的
+        config.setTestOnBorrow(true);
+        JedisPool pool = new JedisPool(config, "127.0.0.1", 6379, 3000);
+        Jedis jedis=pool.getResource();
+        String aa=jedis.get("as");
+        System.out.println(aa);
+
     }
 
     public static boolean method(){
