480bd58641616afd737ee93592980420f38626ca
zhoumiaode <1010467470@qq.com>
2018-12-12 15:09:25 +0800

    fecth test

---
+++ src/main/java/com/example/test/kafka/RR.java
@@ -0,0 +1,16 @@
+package com.example.test.kafka;
+
+/**
+ * @ProjectName: test
+ * @Package: com.example.test.kafka
+ * @ClassName: RR
+ * @Description: java类作用描述
+ * @Author: zhoumiaode
+ * @CreateDate: 2018/12/12 15:07
+ * @UpdateUser: Neil.Zhou
+ * @UpdateDate: 2018/12/12 15:07
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public class RR {
+}
